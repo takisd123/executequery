66ea78eafab9af1251dce3b67cdd5311d79023bf
takisd <takisd123@users.noreply.github.com>
2010-03-16 05:50:49 +0000

    Added help docs to main deploy script.

--- docs/src/build-docs-jar.sh
+++ docs/src/build-docs-jar.sh
@@ -5,8 +5,9 @@ DIST_DIR=../dist
 BUILD_DIR=../build
 
 if [ -d $BUILD_DIR ]; then
-  rm -f $BUILD_DIR/*
+  rm -Rf $BUILD_DIR
 fi
+mkdir $BUILD_DIR
 
 if [ ! -d $DIST_DIR ]; then
   mkdir $DIST_DIR
@@ -16,14 +17,29 @@ if [ -f $DIST_DIR/$JAR_NAME ]; then
   rm -f $DIST_DIR/$JAR_NAME
 fi
 
+echo
 cp -Ra html/ images/ jhelpsearch/ eq.hs helpmap.jhm toc.xml $BUILD_DIR
 for DIR in `find $BUILD_DIR -type d -name 'CVS'`
 do
+  echo -n "."
   rm -Rf $DIR
 done
+echo
+
+for DIR in `find $BUILD_DIR -type d -name '.svn'`
+do
+  echo -n "."
+  rm -Rf $DIR
+done
+echo
+echo
+
+RELEASE_NOTES_TEXT=`cat ../../src/org/executequery/release.notes`
 
 cd $BUILD_DIR
-jar cvf $DIST_DIR/$JAR_NAME html/ images/ jhelpsearch/ eq.hs helpmap.jhm toc.xml 
+sed -e "s/{release-notes}/$RELEASE_NOTES_TEXT/ig" $BUILD_DIR/html/releasenotes.html > $BUILD_DIR/html/releasenotes.html.1
+mv $BUILD_DIR/html/releasenotes.html.1 $BUILD_DIR/html/releasenotes.html
+$JAVA_HOME/bin/jar cvf $DIST_DIR/$JAR_NAME html/ images/ jhelpsearch/ eq.hs helpmap.jhm toc.xml 
 
 #cd ../
 cp -f $DIST_DIR/$JAR_NAME ../
