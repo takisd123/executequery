aa799bbea61a5e076f202d76aa91f6c3daa7ef56
takisd <takisd@78115b7d-f57b-45ba-a8ee-2ae17ea8f87d>
2014-05-09 15:54:09 +0000

    Fixed undo...

--- ExecuteQuery/trunk/src/org/executequery/datasource/ConnectionDataSource.java
+++ ExecuteQuery/trunk/src/org/executequery/datasource/ConnectionDataSource.java
@@ -27,12 +27,10 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
-import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
@@ -311,12 +309,10 @@ public class ConnectionDataSource implements DataSource, DatabaseDataSource {
 	public <T> T unwrap(Class<T> iface) throws SQLException {
 		return null;
 	}
-
+/*
 	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
 		return driver.getParentLogger();
 	}
+*/
 
 }
-
-
-
