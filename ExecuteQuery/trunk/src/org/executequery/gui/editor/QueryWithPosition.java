f326ae69cc5a4eef01fae7963069aa513241194b
takisd <takisd@78115b7d-f57b-45ba-a8ee-2ae17ea8f87d>
2014-10-13 11:23:12 +0000

    tokenizing

--- ExecuteQuery/trunk/src/org/executequery/gui/editor/QueryWithPosition.java
+++ ExecuteQuery/trunk/src/org/executequery/gui/editor/QueryWithPosition.java
@@ -4,16 +4,51 @@ public class QueryWithPosition {
 
     private int start;
     private int end;
+    private int position;
     private String query;
     
-    public QueryWithPosition(int start, int end, String query) {
+    public QueryWithPosition(int position, int start, int end, String query) {
         super();
+        this.position = position;
         this.start = start;
         this.end = end;
         this.query = query;
     }
+
+    public int getPosition() {
+        return position;
+    }
     
     public String getQuery() {
+        
+        /*
+        QueryTokenizer tokenizer = new QueryTokenizer();
+        List<DerivedQuery> queries = tokenizer.tokenize(query);
+        if (queries.size() == 1) {
+            
+            return query;
+        }
+        
+        int length = 0;
+        int offset = position - start;
+        
+        System.out.println("offset " + offset);
+        
+        for (DerivedQuery derivedQuery : queries) {
+            
+            String originalQuery = derivedQuery.getOriginalQuery();
+            length += originalQuery.length();
+
+            System.out.println("length " + length + " - " + originalQuery);
+            
+            if (length >= offset) {
+                
+                return originalQuery;
+            }
+
+        }
+        */
+        
         return query;
     }
     
