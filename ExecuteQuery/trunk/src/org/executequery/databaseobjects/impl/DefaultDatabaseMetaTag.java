27c165b5d544643462b89fde0f121e04c9872443
takisd <takisd@78115b7d-f57b-45ba-a8ee-2ae17ea8f87d>
2015-01-28 12:05:04 +0000

    Added Throwable catch for getFunctions call

--- ExecuteQuery/trunk/src/org/executequery/databaseobjects/impl/DefaultDatabaseMetaTag.java
+++ ExecuteQuery/trunk/src/org/executequery/databaseobjects/impl/DefaultDatabaseMetaTag.java
@@ -393,7 +393,7 @@ public class DefaultDatabaseMetaTag extends AbstractNamedObject
             DatabaseMetaData dmd = getHost().getDatabaseMetaData();        
             return dmd.getFunctions(catalogName, schemaName, null);
         
-        } catch (SQLException e) {
+        } catch (Throwable e) {
             
             // possible SQLFeatureNotSupportedException
             
