1993f50b428690cee66dd9e26a99601257609030
Takis Diakoumis <takisd@executequery.org>
2015-09-24 22:14:12 +1000

    Modified dark theme menus with cleaner border. Added ability to access underlying components on preferences dialog values

---
+++ src/org/underworldlabs/swing/plaf/SimpleMenuBorder.java
@@ -0,0 +1,33 @@
+/*
+ * SimpleMenuBorder.java
+ *
+ * Copyright (C) 2002-2015 Takis Diakoumis
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.underworldlabs.swing.plaf;
+
+import javax.swing.plaf.BorderUIResource;
+import javax.swing.plaf.ColorUIResource;
+
+public class SimpleMenuBorder extends BorderUIResource.LineBorderUIResource {
+    
+    public SimpleMenuBorder(ColorUIResource borderColour) {
+    
+        super(borderColour);
+    }
+
+}
