a72ef2d96bfeb6744e57f66940b33f7592cab847
Takis Diakoumis <takisd@executequery.org>
2015-10-05 22:29:42 +1100

    Modified event thread for logging pane text area

--- src/org/executequery/components/TextAreaLogAppender.java
+++ src/org/executequery/components/TextAreaLogAppender.java
@@ -25,9 +25,11 @@ import java.util.regex.Pattern;
 
 import javax.swing.JTextArea;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
 import org.apache.log4j.spi.LoggingEvent;
+import org.executequery.util.ThreadUtils;
 
 /**
  *
@@ -48,47 +50,60 @@ public class TextAreaLogAppender extends AppenderSkeleton {
 
     /** Creates a new instance of TextAreaLogAppender */
     public TextAreaLogAppender(JTextArea textArea) {
+
         this.textArea = textArea;
         newLineMatcher = Pattern.compile("[\n\r]+").matcher("");
     }
 
     public boolean requiresLayout() {
+
         return true;
     }
 
-    public synchronized void append(LoggingEvent event) {
-        String text = null;
-        Object message = event.getMessage();
-        if (message instanceof String) {
-            text = (String)message;
-            if (text.length() == 1 && Character.isWhitespace(text.charAt(0))) {
-                return;
-            }
-        }
+    public synchronized void append(final LoggingEvent event) {
 
-        text = layout.format(event);
-        newLineMatcher.reset(text);
-        text = newLineMatcher.replaceAll(SPACE).trim();
-
-        textArea.append(text);
-        textArea.append(Layout.LINE_SEP);
-
-        String[] s = event.getThrowableStrRep();
-        if (s != null) {
-            int len = s.length;
-            for(int i = 0; i < len; i++) {
-                textArea.append(s[i]);
+        ThreadUtils.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+            
+                String text = null;
+                Object message = event.getMessage();
+                if (message instanceof String) {
+                
+                    text = (String) message;
+                    if (text.length() == 1 && Character.isWhitespace(text.charAt(0))) {
+                    
+                        return;
+                    }
+        
+                }
+        
+                text = layout.format(event);
+                newLineMatcher.reset(text);
+                text = newLineMatcher.replaceAll(SPACE).trim();
+        
+                textArea.append(text);
                 textArea.append(Layout.LINE_SEP);
-            }
-        }
+        
+                String[] s = event.getThrowableStrRep();
+                if (ArrayUtils.isNotEmpty(s)) {
+        
+                    for(int i = 0; i < s.length; i++) {
+        
+                        textArea.append(s[i]);
+                        textArea.append(Layout.LINE_SEP);
+                    }
+                }
+        
+                textArea.setCaretPosition(textArea.getText().length());
 
-        textArea.setCaretPosition(textArea.getText().length());
+            }
+        });
+        
     }
 
     public void close() {}
 
 }
-
-
-
 
